a72ac4c69f2669deaac5858b511db921089520e6
Diatoufall <diatoufall278@gmail.com>
2025-06-16 15:50:49 -0400

    implementation de app.routes.ts et de characters-list.compoment.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { CharactersListComponent } from './features/characters-list/characters-list.component';
+import { CharacterDetailsComponent } from './features/character-details/character-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: 'characters', 
+    component: CharactersListComponent,
+    title: 'Liste des personnages' 
+  },
+  { 
+    path: 'details', 
+    component: CharacterDetailsComponent,
+    title: 'Détails du personnage' 
+  },
+  { path: '', redirectTo: 'characters', pathMatch: 'full' },
+  { path: '**', redirectTo: 'characters' } // Redirection pour les routes inconnues
+];
