6b7d93846bba0c91b88b7e3ad391cba92754a5ad
Diatoufall <diatoufall278@gmail.com>
2025-06-16 16:16:14 -0400

    implemetation refaite de config.ts,charater-details.compoment.html et details.compoment.ts

--- src/app/features/character-details/character-details.component.ts
+++ src/app/features/character-details/character-details.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RickAndMortyService } from '../../core/services/rick-and-morty.service';
 
 @Component({
   selector: 'app-character-details',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './character-details.component.html',
-  styleUrl: './character-details.component.scss'
+  styleUrls: ['./character-details.component.scss']
 })
 export class CharacterDetailsComponent {
+  characterId: number | null = null;
+  character: any = null;
+  error: string | null = null;
 
+  constructor(private rickAndMortyService: RickAndMortyService) {}
+
+  searchCharacter(): void {
+    if (!this.characterId || this.characterId <= 0) {
+      this.error = "ID invalide";
+      return;
+    }
+
+    this.error = null;
+    this.character = null;
+
+    this.rickAndMortyService.getCharacterById(this.characterId).subscribe({
+      next: (character) => {
+        this.character = character;
+      },
+      error: () => {
+        this.error = "Personnage non trouvé";
+      }
+    });
+  }
 }
